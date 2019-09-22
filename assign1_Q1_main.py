2fa9df94ee4d988f42d0b7f71cb974165904a5cd
felipe-clark <felipe.clark@gmail.com>
2019-09-22 11:42:26 -0400

    Progress counter for file saving operations and cleanup

--- assign1_Q1_main.py
+++ assign1_Q1_main.py
@@ -9,8 +9,10 @@ total_pixels_frame = x_res * y_res
 bytes_frame = (int) (3 * (x_res * y_res) / 2)
 
 y_frame = np.empty((0,0))
-u_frame = np.empty((y_res, x_res, number_frames))
-v_frame = np.empty((y_res, x_res, number_frames))
+u_frame = np.empty((number_frames, y_res, x_res), dtype=int)
+#u_frame = [[[0 for col in range(y_res)] for col in range(x_res)] for row in range(number_frames)]
+v_frame = np.empty((number_frames, y_res, x_res), dtype=int)
+#v_frame = [[[0 for col in range(y_res)] for col in range(x_res)] for row in range(number_frames)]
 
 yuv_file = open("foreman_cif.yuv","rb")
 
@@ -22,25 +24,25 @@ for frame in range(number_frames) :
     for y_it in range(y_res) :
         u_it = 0
         for x_it in range(x_res) :
-            it_offset = y_it * x_res + x_it
             u_offset = total_pixels_frame + u_it
             v_offset = (int) (u_offset + total_pixels_frame / 4)
             
             if y_it % 2 == 0 :
-                if x_it % 2 == 0 :#if it_offset % 2 == 0 :
-                    u_frame[y_it][x_it][frame] = int.from_bytes(raw[u_offset : u_offset + 1], byteorder=sys.byteorder)
-                    v_frame[y_it][x_it][frame] = int.from_bytes(raw[v_offset : v_offset + 1], byteorder=sys.byteorder)
+                if x_it % 2 == 0 :
+                    u_frame[frame][y_it][x_it] = int.from_bytes((raw[u_offset : u_offset + 1]), byteorder=sys.byteorder)
+                    v_frame[frame][y_it][x_it] = int.from_bytes((raw[v_offset : v_offset + 1]), byteorder=sys.byteorder)
                     u_it = u_it + 1
+                    #print(u_frame[y_it][x_it][frame], int.from_bytes((raw[u_offset : u_offset + 1]), byteorder=sys.byteorder), raw[u_offset : u_offset + 1])
                 else :
-                    u_frame[y_it][x_it][frame] = u_frame[y_it][x_it - 1][frame]
-                    v_frame[y_it][x_it][frame] = v_frame[y_it][x_it - 1][frame]
+                    u_frame[frame][y_it][x_it] = u_frame[frame][y_it][x_it - 1]
+                    v_frame[frame][y_it][x_it] = v_frame[frame][y_it][x_it - 1]
             else :
-                u_frame[y_it][x_it][frame] = u_frame[y_it - 1][x_it][frame]
-                v_frame[y_it][x_it][frame] = v_frame[y_it - 1][x_it][frame]
+                u_frame[frame][y_it][x_it] = u_frame[frame][y_it - 1][x_it]
+                v_frame[frame][y_it][x_it] = v_frame[frame][y_it - 1][x_it]
           
     progress = (int) (frame / (number_frames - 1) * 100)
     if progress % 10 == 0 :
-        sys.stdout.write("Progress: %d%%   \r" % (progress) )
+        sys.stdout.write("Converting: %d%%   \r" % (progress) )
         sys.stdout.flush()
     
 yuv_file.close()
@@ -53,12 +55,17 @@ for frame in range(number_frames) :
     
     for y_it in range(y_res) :    
         for x_it in range(x_res) :
-            converted.write((int)(u_frame[y_it][x_it][frame]).to_bytes(1, byteorder=sys.byteorder))
+            converted.write((int)(u_frame[frame][y_it][x_it]).to_bytes(1, byteorder=sys.byteorder))
             
     for y_it in range(y_res) :    
         for x_it in range(x_res) :
-            converted.write((int)(v_frame[y_it][x_it][frame]).to_bytes(1, byteorder=sys.byteorder))
+            converted.write((int)(v_frame[frame][y_it][x_it]).to_bytes(1, byteorder=sys.byteorder))
     
+    progress = (int) (frame / (number_frames - 1) * 100)
+    if progress % 10 == 0 :
+        sys.stdout.write("Saving file: %d%%   \r" % (progress) )
+        sys.stdout.flush()
+        
 converted.close()
 
 
