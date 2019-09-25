517da7456d4432da35996611c93dd67aa0ea2ae3
haldarritam <haldarritam@gmail.com>
2019-09-24 22:29:44 -0400

    Modified the RGB pixel arrangement to Boxed row flat pixel format and currently clamping the RGB values.

--- assign1_Q1_part_b.py
+++ assign1_Q1_part_b.py
@@ -11,26 +11,35 @@ bytes_frame = (int) (3 * (x_res * y_res) / 2)
 yuv_to_rgb = np.array([1.164, 0, 1.596, 1.164, 0.392, 0.813, 1.164, 2.017, 0]).reshape(3,3)
 yuv_array = np.empty((3,1))
 
-rgb_pixels = np.empty((3*total_pixels_frame))
+rgb_pixels = np.empty((y_res, 3*x_res), dtype=int)
 
 converted = open("./videos/foreman_cif_444.yuv", "rb")
 
 for frame in range(number_frames) :
-        raw = converted.read(bytes_frame)
-        for it in range(total_pixels_frame) :
-                u_it = it + total_pixels_frame
-                v_it = u_it + total_pixels_frame
+    raw = converted.read(bytes_frame)
+    for y_it in range(y_res):
+        x_rgb_it = 0
+        for x_it in range(x_res):
+            it = (y_it * x_res) + x_it
+            u_it = it + total_pixels_frame
+            v_it = u_it + total_pixels_frame
 
-                yuv_array[0] = int.from_bytes((raw[it:it+1]), byteorder=sys.byteorder) - 16
-                yuv_array[1] = int.from_bytes((raw[u_it:u_it+1]), byteorder=sys.byteorder) - 128
-                yuv_array[2] = int.from_bytes((raw[v_it:v_it+1]), byteorder=sys.byteorder) - 128
+            yuv_array[0] = int.from_bytes((raw[it:it+1]), byteorder=sys.byteorder) - 16
+            yuv_array[1] = int.from_bytes((raw[u_it:u_it+1]), byteorder=sys.byteorder) - 128
+            yuv_array[2] = int.from_bytes((raw[v_it:v_it+1]), byteorder=sys.byteorder) - 128
 
-                rgb = np.dot(yuv_to_rgb, yuv_array)
+            rgb = np.dot(yuv_to_rgb, yuv_array)
 
-                rgb_pixels[it] = rgb[0]
-                rgb_pixels[u_it] = rgb[1]
-                rgb_pixels[v_it] = rgb[2]
+            rgb[0] = max(0, min(rgb[0], 255))
+            rgb[1] = max(0, min(rgb[1], 255))
+            rgb[2] = max(0, min(rgb[2], 255))
 
-        png.from_array(rgb_pixels, mode='RGB', info={'height': 288, 'width': 352, 'bitdepth':3}).save('foo.png')
+            rgb_pixels[y_it][x_rgb_it]     = rgb[0]
+            rgb_pixels[y_it][x_rgb_it + 1] = rgb[1]
+            rgb_pixels[y_it][x_rgb_it + 2] = rgb[2]
+
+            x_rgb_it = x_rgb_it + 3
+
+    png.from_array(rgb_pixels, mode='RGB', info={'height': y_res, 'width': x_res, 'bitdepth':8}).save('foo.png')
 
 converted.close()
