e0149c4a5f0110e8bc809fbf044bae84b3c817d9
haldarritam <haldarritam@gmail.com>
2019-10-20 18:08:29 -0400

    Adding q3 experiment results for hall.

--- data_generation/SAD_per_frame.py
+++ data_generation/SAD_per_frame.py
@@ -63,26 +63,31 @@ if __name__ == "__main__":
 
   plt.rc('font', **font)
 
-  in_file_1 = "./videos/black_and_white.yuv"
-  y_res = 288
-  x_res = 352
+  # in_file_1 = "./videos/black_and_white.yuv"
+  # y_res = 288
+  # x_res = 352
+  # number_frames = 10
+
+  in_file_1 = "./videos/hall_qcif_bw.yuv"
+  y_res = 144
+  x_res = 176
   number_frames = 10
 
   # ############################################################################
   # ######################## Experiment for varying i ##########################
   # ############################################################################
   # # i=2
-  # in_file_2 = "./videos/q3_decoded_i2.yuv"
+  # in_file_2 = "./videos/q3_i2_decoded.yuv"
   # SAD_1 = sad_per_frame(in_file_1, in_file_2, y_res, x_res, number_frames)
   # SAD_1[:] = [x / 100000 for x in SAD_1]
 
   # # i=8
-  # in_file_2 = "./videos/q3_decoded_i8.yuv"
+  # in_file_2 = "./videos/q3_i8_decoded.yuv"
   # SAD_2 = sad_per_frame(in_file_1, in_file_2, y_res, x_res, number_frames)
   # SAD_2[:] = [x / 100000 for x in SAD_2]
 
   # # i=64
-  # in_file_2 = "./videos/q3_decoded_i64.yuv"
+  # in_file_2 = "./videos/q3_i64_decoded.yuv"
   # SAD_3 = sad_per_frame(in_file_1, in_file_2, y_res, x_res, number_frames)
   # SAD_3[:] = [x / 100000 for x in SAD_3]
   # ############################################################################
@@ -111,72 +116,89 @@ if __name__ == "__main__":
   # ############################################################################
   # ######################## Experiment for varying n ##########################
   # ############################################################################
-  # # n=1
-  # in_file_2 = "./videos/q3_n1_decoded.yuv"
-  # SAD_1 = sad_per_frame(in_file_1, in_file_2, y_res, x_res, number_frames)
-  # SAD_1[:] = [x / 100000 for x in SAD_1]
+  # n=1
+  in_file_2 = "./videos/q3_n1_decoded.yuv"
+  SAD_1 = sad_per_frame(in_file_1, in_file_2, y_res, x_res, number_frames)
+  SAD_1[:] = [x / 100000 for x in SAD_1]
 
-  # # n=2
-  # in_file_2 = "./videos/q3_n2_decoded.yuv"
-  # SAD_2 = sad_per_frame(in_file_1, in_file_2, y_res, x_res, number_frames)
-  # SAD_2[:] = [x / 100000 for x in SAD_2]
+  # n=2
+  in_file_2 = "./videos/q3_n2_decoded.yuv"
+  SAD_2 = sad_per_frame(in_file_1, in_file_2, y_res, x_res, number_frames)
+  SAD_2[:] = [x / 100000 for x in SAD_2]
 
-  # # n=3
-  # in_file_2 = "./videos/q3_n3_decoded.yuv"
-  # SAD_3 = sad_per_frame(in_file_1, in_file_2, y_res, x_res, number_frames)
-  # SAD_3[:] = [x / 100000 for x in SAD_3]
+  # n=3
+  in_file_2 = "./videos/q3_n3_decoded.yuv"
+  SAD_3 = sad_per_frame(in_file_1, in_file_2, y_res, x_res, number_frames)
+  SAD_3[:] = [x / 100000 for x in SAD_3]
   # ############################################################################
   # ############################################################################
 
-  # ############################################################################
-  # ######################## Experiment for varying n ##########################
-  # ############################################################################
-
-  total_pixels_frame = x_res * y_res
-  y_frame_1 = np.empty((number_frames, y_res, x_res), dtype=int)
-
-  yuv_file_1 = open(in_file_1,"rb")
-
-  for frame in range(number_frames):
-    raw_1 = yuv_file_1.read(total_pixels_frame)
-    for y_it in range(y_res) :    
-      for x_it in range(x_res):
-        it = (y_it * x_res) + x_it
-        y_frame_1[frame][y_it][x_it] = int.from_bytes((raw_1[it: it + 1]), byteorder=sys.byteorder)
-        
-  yuv_file_1.close()
-
-  psnr = []
-  file_size = []
-  for ip in [1, 4, 10]:
-    psnr_temp = []
-    file_size_temp = []
-    for QP in range(0, 12):
-      in_file_2 = "./videos/report/q4/i16/q4_ip" + str(ip) + "_qp" + str(QP) + "_decoded.yuv"
-      psnr_temp += [PSNR(y_frame_1, in_file_2, y_res, x_res, number_frames)]
-
-      encoded_in_file_2 = "./videos/report/q4/i16/q4_ip" + str(ip) + "_qp" + str(QP) + "_encoded.far"
-      file_size_temp += [os.path.getsize(encoded_in_file_2) * 8]
-
-    psnr += [psnr_temp]
-    file_size += [file_size_temp]
-
-
-  # ############################################################################
-  # ############################################################################
-
-
+  frames = range(1,11)
   fig, ax = plt.subplots()
 
-  ax.plot(file_size[0], psnr[0], label='I_Period=1')
-  ax.plot(file_size[1], psnr[1], label='I_Period=4')
-  ax.plot(file_size[2], psnr[2], label='I_Period=10')
+  ax.plot(frames, SAD_1, label='n=1')
+  ax.plot(frames, SAD_2, label='n=2')
+  ax.plot(frames, SAD_3, label='n=3')
   
   
   
-  ax.set(xlabel='Size in bits', ylabel='PSNR (dB)')
+  ax.set(xlabel='Frame', ylabel=r'SAD $(\times 10^6)$')
   ax.grid()
   ax.legend()
 
   # fig.savefig("test.png")
   plt.show()
+
+
+  # ############################################################################
+  # ############################# Experiment for Q4 ############################
+  # ############################################################################
+
+  # total_pixels_frame = x_res * y_res
+  # y_frame_1 = np.empty((number_frames, y_res, x_res), dtype=int)
+
+  # yuv_file_1 = open(in_file_1,"rb")
+
+  # for frame in range(number_frames):
+  #   raw_1 = yuv_file_1.read(total_pixels_frame)
+  #   for y_it in range(y_res) :    
+  #     for x_it in range(x_res):
+  #       it = (y_it * x_res) + x_it
+  #       y_frame_1[frame][y_it][x_it] = int.from_bytes((raw_1[it: it + 1]), byteorder=sys.byteorder)
+        
+  # yuv_file_1.close()
+
+  # psnr = []
+  # file_size = []
+  # for ip in [1, 4, 10]:
+  #   psnr_temp = []
+  #   file_size_temp = []
+  #   for QP in range(0, 12):
+  #     in_file_2 = "./videos/report/q4/i16/q4_ip" + str(ip) + "_qp" + str(QP) + "_decoded.yuv"
+  #     psnr_temp += [PSNR(y_frame_1, in_file_2, y_res, x_res, number_frames)]
+
+  #     encoded_in_file_2 = "./videos/report/q4/i16/q4_ip" + str(ip) + "_qp" + str(QP) + "_encoded.far"
+  #     file_size_temp += [os.path.getsize(encoded_in_file_2) * 8]
+
+  #   psnr += [psnr_temp]
+  #   file_size += [file_size_temp]
+
+
+  # # ############################################################################
+  # # ############################################################################
+
+
+  # fig, ax = plt.subplots()
+
+  # ax.plot(file_size[0], psnr[0], label='I_Period=1')
+  # ax.plot(file_size[1], psnr[1], label='I_Period=4')
+  # ax.plot(file_size[2], psnr[2], label='I_Period=10')
+  
+  
+  
+  # ax.set(xlabel='Size in bits', ylabel='PSNR (dB)')
+  # ax.grid()
+  # ax.legend()
+
+  # # fig.savefig("test.png")
+  # plt.show()
--- experiment.sh
+++ experiment.sh
@@ -66,75 +66,75 @@
 # python main.py -in ./videos/black_and_white.yuv -out ./videos/report/q4_ip10_qp9.yuv -y_res 288 -x_res 352 -frames 10 -o both -q 4 -i 8 -r 2 -ip 10 -QP 9&
 # python main.py -in ./videos/black_and_white.yuv -out ./videos/report/q4_ip10_qp10.yuv -y_res 288 -x_res 352 -frames 10 -o both -q 4 -i 8 -r 2 -ip 10 -QP 10&
 
-# i = 16
-# GOP is IIII
-python main.py -in ./videos/black_and_white.yuv -out ./videos/report/q4_ip1_qp0.yuv -y_res 288 -x_res 352 -frames 10 -o both -q 4 -i 16 -r 2 -ip 1 -QP 0&
-python main.py -in ./videos/black_and_white.yuv -out ./videos/report/q4_ip1_qp1.yuv -y_res 288 -x_res 352 -frames 10 -o both -q 4 -i 16 -r 2 -ip 1 -QP 1&
-python main.py -in ./videos/black_and_white.yuv -out ./videos/report/q4_ip1_qp2.yuv -y_res 288 -x_res 352 -frames 10 -o both -q 4 -i 16 -r 2 -ip 1 -QP 2&
-python main.py -in ./videos/black_and_white.yuv -out ./videos/report/q4_ip1_qp3.yuv -y_res 288 -x_res 352 -frames 10 -o both -q 4 -i 16 -r 2 -ip 1 -QP 3&
-python main.py -in ./videos/black_and_white.yuv -out ./videos/report/q4_ip1_qp4.yuv -y_res 288 -x_res 352 -frames 10 -o both -q 4 -i 16 -r 2 -ip 1 -QP 4&
-python main.py -in ./videos/black_and_white.yuv -out ./videos/report/q4_ip1_qp5.yuv -y_res 288 -x_res 352 -frames 10 -o both -q 4 -i 16 -r 2 -ip 1 -QP 5&
-python main.py -in ./videos/black_and_white.yuv -out ./videos/report/q4_ip1_qp6.yuv -y_res 288 -x_res 352 -frames 10 -o both -q 4 -i 16 -r 2 -ip 1 -QP 6&
-python main.py -in ./videos/black_and_white.yuv -out ./videos/report/q4_ip1_qp7.yuv -y_res 288 -x_res 352 -frames 10 -o both -q 4 -i 16 -r 2 -ip 1 -QP 7&
-python main.py -in ./videos/black_and_white.yuv -out ./videos/report/q4_ip1_qp8.yuv -y_res 288 -x_res 352 -frames 10 -o both -q 4 -i 16 -r 2 -ip 1 -QP 8&
-python main.py -in ./videos/black_and_white.yuv -out ./videos/report/q4_ip1_qp9.yuv -y_res 288 -x_res 352 -frames 10 -o both -q 4 -i 16 -r 2 -ip 1 -QP 9&
-python main.py -in ./videos/black_and_white.yuv -out ./videos/report/q4_ip1_qp10.yuv -y_res 288 -x_res 352 -frames 10 -o both -q 4 -i 16 -r 2 -ip 1 -QP 10&
-python main.py -in ./videos/black_and_white.yuv -out ./videos/report/q4_ip1_qp11.yuv -y_res 288 -x_res 352 -frames 10 -o both -q 4 -i 16 -r 2 -ip 1 -QP 11&
+# # i = 16
+# # GOP is IIII
+# python main.py -in ./videos/black_and_white.yuv -out ./videos/report/q4_ip1_qp0.yuv -y_res 288 -x_res 352 -frames 10 -o both -q 4 -i 16 -r 2 -ip 1 -QP 0&
+# python main.py -in ./videos/black_and_white.yuv -out ./videos/report/q4_ip1_qp1.yuv -y_res 288 -x_res 352 -frames 10 -o both -q 4 -i 16 -r 2 -ip 1 -QP 1&
+# python main.py -in ./videos/black_and_white.yuv -out ./videos/report/q4_ip1_qp2.yuv -y_res 288 -x_res 352 -frames 10 -o both -q 4 -i 16 -r 2 -ip 1 -QP 2&
+# python main.py -in ./videos/black_and_white.yuv -out ./videos/report/q4_ip1_qp3.yuv -y_res 288 -x_res 352 -frames 10 -o both -q 4 -i 16 -r 2 -ip 1 -QP 3&
+# python main.py -in ./videos/black_and_white.yuv -out ./videos/report/q4_ip1_qp4.yuv -y_res 288 -x_res 352 -frames 10 -o both -q 4 -i 16 -r 2 -ip 1 -QP 4&
+# python main.py -in ./videos/black_and_white.yuv -out ./videos/report/q4_ip1_qp5.yuv -y_res 288 -x_res 352 -frames 10 -o both -q 4 -i 16 -r 2 -ip 1 -QP 5&
+# python main.py -in ./videos/black_and_white.yuv -out ./videos/report/q4_ip1_qp6.yuv -y_res 288 -x_res 352 -frames 10 -o both -q 4 -i 16 -r 2 -ip 1 -QP 6&
+# python main.py -in ./videos/black_and_white.yuv -out ./videos/report/q4_ip1_qp7.yuv -y_res 288 -x_res 352 -frames 10 -o both -q 4 -i 16 -r 2 -ip 1 -QP 7&
+# python main.py -in ./videos/black_and_white.yuv -out ./videos/report/q4_ip1_qp8.yuv -y_res 288 -x_res 352 -frames 10 -o both -q 4 -i 16 -r 2 -ip 1 -QP 8&
+# python main.py -in ./videos/black_and_white.yuv -out ./videos/report/q4_ip1_qp9.yuv -y_res 288 -x_res 352 -frames 10 -o both -q 4 -i 16 -r 2 -ip 1 -QP 9&
+# python main.py -in ./videos/black_and_white.yuv -out ./videos/report/q4_ip1_qp10.yuv -y_res 288 -x_res 352 -frames 10 -o both -q 4 -i 16 -r 2 -ip 1 -QP 10&
+# python main.py -in ./videos/black_and_white.yuv -out ./videos/report/q4_ip1_qp11.yuv -y_res 288 -x_res 352 -frames 10 -o both -q 4 -i 16 -r 2 -ip 1 -QP 11&
 
-# GOP is IPPPIPPPI...
+# # GOP is IPPPIPPPI...
 
-python main.py -in ./videos/black_and_white.yuv -out ./videos/report/q4_ip4_qp0.yuv -y_res 288 -x_res 352 -frames 10 -o both -q 4 -i 16 -r 2 -ip 4 -QP 0&
-python main.py -in ./videos/black_and_white.yuv -out ./videos/report/q4_ip4_qp1.yuv -y_res 288 -x_res 352 -frames 10 -o both -q 4 -i 16 -r 2 -ip 4 -QP 1&
-python main.py -in ./videos/black_and_white.yuv -out ./videos/report/q4_ip4_qp2.yuv -y_res 288 -x_res 352 -frames 10 -o both -q 4 -i 16 -r 2 -ip 4 -QP 2&
-python main.py -in ./videos/black_and_white.yuv -out ./videos/report/q4_ip4_qp3.yuv -y_res 288 -x_res 352 -frames 10 -o both -q 4 -i 16 -r 2 -ip 4 -QP 3&
-python main.py -in ./videos/black_and_white.yuv -out ./videos/report/q4_ip4_qp4.yuv -y_res 288 -x_res 352 -frames 10 -o both -q 4 -i 16 -r 2 -ip 4 -QP 4&
-python main.py -in ./videos/black_and_white.yuv -out ./videos/report/q4_ip4_qp5.yuv -y_res 288 -x_res 352 -frames 10 -o both -q 4 -i 16 -r 2 -ip 4 -QP 5&
-python main.py -in ./videos/black_and_white.yuv -out ./videos/report/q4_ip4_qp6.yuv -y_res 288 -x_res 352 -frames 10 -o both -q 4 -i 16 -r 2 -ip 4 -QP 6&
-python main.py -in ./videos/black_and_white.yuv -out ./videos/report/q4_ip4_qp7.yuv -y_res 288 -x_res 352 -frames 10 -o both -q 4 -i 16 -r 2 -ip 4 -QP 7&
-python main.py -in ./videos/black_and_white.yuv -out ./videos/report/q4_ip4_qp8.yuv -y_res 288 -x_res 352 -frames 10 -o both -q 4 -i 16 -r 2 -ip 4 -QP 8&
-python main.py -in ./videos/black_and_white.yuv -out ./videos/report/q4_ip4_qp9.yuv -y_res 288 -x_res 352 -frames 10 -o both -q 4 -i 16 -r 2 -ip 4 -QP 9&
-python main.py -in ./videos/black_and_white.yuv -out ./videos/report/q4_ip4_qp10.yuv -y_res 288 -x_res 352 -frames 10 -o both -q 4 -i 16 -r 2 -ip 4 -QP 10&
-python main.py -in ./videos/black_and_white.yuv -out ./videos/report/q4_ip4_qp11.yuv -y_res 288 -x_res 352 -frames 10 -o both -q 4 -i 16 -r 2 -ip 4 -QP 11&
+# python main.py -in ./videos/black_and_white.yuv -out ./videos/report/q4_ip4_qp0.yuv -y_res 288 -x_res 352 -frames 10 -o both -q 4 -i 16 -r 2 -ip 4 -QP 0&
+# python main.py -in ./videos/black_and_white.yuv -out ./videos/report/q4_ip4_qp1.yuv -y_res 288 -x_res 352 -frames 10 -o both -q 4 -i 16 -r 2 -ip 4 -QP 1&
+# python main.py -in ./videos/black_and_white.yuv -out ./videos/report/q4_ip4_qp2.yuv -y_res 288 -x_res 352 -frames 10 -o both -q 4 -i 16 -r 2 -ip 4 -QP 2&
+# python main.py -in ./videos/black_and_white.yuv -out ./videos/report/q4_ip4_qp3.yuv -y_res 288 -x_res 352 -frames 10 -o both -q 4 -i 16 -r 2 -ip 4 -QP 3&
+# python main.py -in ./videos/black_and_white.yuv -out ./videos/report/q4_ip4_qp4.yuv -y_res 288 -x_res 352 -frames 10 -o both -q 4 -i 16 -r 2 -ip 4 -QP 4&
+# python main.py -in ./videos/black_and_white.yuv -out ./videos/report/q4_ip4_qp5.yuv -y_res 288 -x_res 352 -frames 10 -o both -q 4 -i 16 -r 2 -ip 4 -QP 5&
+# python main.py -in ./videos/black_and_white.yuv -out ./videos/report/q4_ip4_qp6.yuv -y_res 288 -x_res 352 -frames 10 -o both -q 4 -i 16 -r 2 -ip 4 -QP 6&
+# python main.py -in ./videos/black_and_white.yuv -out ./videos/report/q4_ip4_qp7.yuv -y_res 288 -x_res 352 -frames 10 -o both -q 4 -i 16 -r 2 -ip 4 -QP 7&
+# python main.py -in ./videos/black_and_white.yuv -out ./videos/report/q4_ip4_qp8.yuv -y_res 288 -x_res 352 -frames 10 -o both -q 4 -i 16 -r 2 -ip 4 -QP 8&
+# python main.py -in ./videos/black_and_white.yuv -out ./videos/report/q4_ip4_qp9.yuv -y_res 288 -x_res 352 -frames 10 -o both -q 4 -i 16 -r 2 -ip 4 -QP 9&
+# python main.py -in ./videos/black_and_white.yuv -out ./videos/report/q4_ip4_qp10.yuv -y_res 288 -x_res 352 -frames 10 -o both -q 4 -i 16 -r 2 -ip 4 -QP 10&
+# python main.py -in ./videos/black_and_white.yuv -out ./videos/report/q4_ip4_qp11.yuv -y_res 288 -x_res 352 -frames 10 -o both -q 4 -i 16 -r 2 -ip 4 -QP 11&
 
-# GOP is IPPPPPPPPPIPPPPPPPPPI...
+# # GOP is IPPPPPPPPPIPPPPPPPPPI...
 
-python main.py -in ./videos/black_and_white.yuv -out ./videos/report/q4_ip10_qp0.yuv -y_res 288 -x_res 352 -frames 10 -o both -q 4 -i 16 -r 2 -ip 10 -QP 0&
-python main.py -in ./videos/black_and_white.yuv -out ./videos/report/q4_ip10_qp1.yuv -y_res 288 -x_res 352 -frames 10 -o both -q 4 -i 16 -r 2 -ip 10 -QP 1&
-python main.py -in ./videos/black_and_white.yuv -out ./videos/report/q4_ip10_qp2.yuv -y_res 288 -x_res 352 -frames 10 -o both -q 4 -i 16 -r 2 -ip 10 -QP 2&
-python main.py -in ./videos/black_and_white.yuv -out ./videos/report/q4_ip10_qp3.yuv -y_res 288 -x_res 352 -frames 10 -o both -q 4 -i 16 -r 2 -ip 10 -QP 3&
-python main.py -in ./videos/black_and_white.yuv -out ./videos/report/q4_ip10_qp4.yuv -y_res 288 -x_res 352 -frames 10 -o both -q 4 -i 16 -r 2 -ip 10 -QP 4&
-python main.py -in ./videos/black_and_white.yuv -out ./videos/report/q4_ip10_qp5.yuv -y_res 288 -x_res 352 -frames 10 -o both -q 4 -i 16 -r 2 -ip 10 -QP 5&
-python main.py -in ./videos/black_and_white.yuv -out ./videos/report/q4_ip10_qp6.yuv -y_res 288 -x_res 352 -frames 10 -o both -q 4 -i 16 -r 2 -ip 10 -QP 6&
-python main.py -in ./videos/black_and_white.yuv -out ./videos/report/q4_ip10_qp7.yuv -y_res 288 -x_res 352 -frames 10 -o both -q 4 -i 16 -r 2 -ip 10 -QP 7&
-python main.py -in ./videos/black_and_white.yuv -out ./videos/report/q4_ip10_qp8.yuv -y_res 288 -x_res 352 -frames 10 -o both -q 4 -i 16 -r 2 -ip 10 -QP 8&
-python main.py -in ./videos/black_and_white.yuv -out ./videos/report/q4_ip10_qp9.yuv -y_res 288 -x_res 352 -frames 10 -o both -q 4 -i 16 -r 2 -ip 10 -QP 9&
-python main.py -in ./videos/black_and_white.yuv -out ./videos/report/q4_ip10_qp10.yuv -y_res 288 -x_res 352 -frames 10 -o both -q 4 -i 16 -r 2 -ip 10 -QP 10&
-python main.py -in ./videos/black_and_white.yuv -out ./videos/report/q4_ip10_qp11.yuv -y_res 288 -x_res 352 -frames 10 -o both -q 4 -i 16 -r 2 -ip 10 -QP 11&
+# python main.py -in ./videos/black_and_white.yuv -out ./videos/report/q4_ip10_qp0.yuv -y_res 288 -x_res 352 -frames 10 -o both -q 4 -i 16 -r 2 -ip 10 -QP 0&
+# python main.py -in ./videos/black_and_white.yuv -out ./videos/report/q4_ip10_qp1.yuv -y_res 288 -x_res 352 -frames 10 -o both -q 4 -i 16 -r 2 -ip 10 -QP 1&
+# python main.py -in ./videos/black_and_white.yuv -out ./videos/report/q4_ip10_qp2.yuv -y_res 288 -x_res 352 -frames 10 -o both -q 4 -i 16 -r 2 -ip 10 -QP 2&
+# python main.py -in ./videos/black_and_white.yuv -out ./videos/report/q4_ip10_qp3.yuv -y_res 288 -x_res 352 -frames 10 -o both -q 4 -i 16 -r 2 -ip 10 -QP 3&
+# python main.py -in ./videos/black_and_white.yuv -out ./videos/report/q4_ip10_qp4.yuv -y_res 288 -x_res 352 -frames 10 -o both -q 4 -i 16 -r 2 -ip 10 -QP 4&
+# python main.py -in ./videos/black_and_white.yuv -out ./videos/report/q4_ip10_qp5.yuv -y_res 288 -x_res 352 -frames 10 -o both -q 4 -i 16 -r 2 -ip 10 -QP 5&
+# python main.py -in ./videos/black_and_white.yuv -out ./videos/report/q4_ip10_qp6.yuv -y_res 288 -x_res 352 -frames 10 -o both -q 4 -i 16 -r 2 -ip 10 -QP 6&
+# python main.py -in ./videos/black_and_white.yuv -out ./videos/report/q4_ip10_qp7.yuv -y_res 288 -x_res 352 -frames 10 -o both -q 4 -i 16 -r 2 -ip 10 -QP 7&
+# python main.py -in ./videos/black_and_white.yuv -out ./videos/report/q4_ip10_qp8.yuv -y_res 288 -x_res 352 -frames 10 -o both -q 4 -i 16 -r 2 -ip 10 -QP 8&
+# python main.py -in ./videos/black_and_white.yuv -out ./videos/report/q4_ip10_qp9.yuv -y_res 288 -x_res 352 -frames 10 -o both -q 4 -i 16 -r 2 -ip 10 -QP 9&
+# python main.py -in ./videos/black_and_white.yuv -out ./videos/report/q4_ip10_qp10.yuv -y_res 288 -x_res 352 -frames 10 -o both -q 4 -i 16 -r 2 -ip 10 -QP 10&
+# python main.py -in ./videos/black_and_white.yuv -out ./videos/report/q4_ip10_qp11.yuv -y_res 288 -x_res 352 -frames 10 -o both -q 4 -i 16 -r 2 -ip 10 -QP 11&
 
 # ######################################################################
 # Generating for various i values (Q3) - Hall
 # ######################################################################
-python main.py -in ./videos/hall_qcif_bw.yuv -out ./videos/q3_r1_hall.yuv -y_res 144 -x_res 176 -frames 10 -o both -q 3 -i 2 -n 3 -r 4 &
+python main.py -in ./videos/hall_qcif_bw.yuv -out ./videos/q3_i2.yuv -y_res 144 -x_res 176 -frames 10 -o both -q 3 -i 2 -n 3 -r 4 &
 
-python main.py -in ./videos/hall_qcif_bw.yuv -out ./videos/q3_r4_hall.yuv -y_res 144 -x_res 176 -frames 10 -o both -q 3 -i 8 -n 3 -r 4 &
+python main.py -in ./videos/hall_qcif_bw.yuv -out ./videos/q3_i8.yuv -y_res 144 -x_res 176 -frames 10 -o both -q 3 -i 8 -n 3 -r 4 &
 
-python main.py -in ./videos/hall_qcif_bw.yuv -out ./videos/q3_r8_hal.yuv -y_res 144 -x_res 176 -frames 10 -o both -q 3 -i 64 -n 3 -r 4 &
+python main.py -in ./videos/hall_qcif_bw.yuv -out ./videos/q3_i64.yuv -y_res 144 -x_res 176 -frames 10 -o both -q 3 -i 64 -n 3 -r 4 &
 
 # ######################################################################
 # Generating for various r values (Q3) - Hall
 # ######################################################################
-# python main.py -in ./videos/hall_qcif_bw.yuv -out ./videos/q3_r1_hall.yuv -y_res 144 -x_res 176 -frames 10 -o both -q 3 -i 8 -n 3 -r 1 &
+python main.py -in ./videos/hall_qcif_bw.yuv -out ./videos/q3_r1.yuv -y_res 144 -x_res 176 -frames 10 -o both -q 3 -i 8 -n 3 -r 1 &
 
-# python main.py -in ./videos/hall_qcif_bw.yuv -out ./videos/q3_r4_hall.yuv -y_res 144 -x_res 176 -frames 10 -o both -q 3 -i 8 -n 3 -r 4 &
+python main.py -in ./videos/hall_qcif_bw.yuv -out ./videos/q3_r4.yuv -y_res 144 -x_res 176 -frames 10 -o both -q 3 -i 8 -n 3 -r 4 &
 
-# python main.py -in ./videos/hall_qcif_bw.yuv -out ./videos/q3_r8_hall.yuv -y_res 144 -x_res 176 -frames 10 -o both -q 3 -i 8 -n 3 -r 8 &
+python main.py -in ./videos/hall_qcif_bw.yuv -out ./videos/q3_r8.yuv -y_res 144 -x_res 176 -frames 10 -o both -q 3 -i 8 -n 3 -r 8 &
 
 
 # ######################################################################
 # Generating for various n values (Q3) - Hall
 # ######################################################################
-# python main.py -in ./videos/hall_qcif_bw.yuv -out ./videos/q3_n1_hall.yuv -y_res 144 -x_res 176 -frames 10 -o both -q 3 -i 8 -n 1 -r 4 &
+python main.py -in ./videos/hall_qcif_bw.yuv -out ./videos/q3_n1.yuv -y_res 144 -x_res 176 -frames 10 -o both -q 3 -i 8 -n 1 -r 4 &
 
-# python main.py -in ./videos/hall_qcif_bw.yuv -out ./videos/q3_n2_hall.yuv -y_res 144 -x_res 176 -frames 10 -o both -q 3 -i 8 -n 2 -r 4 &
+python main.py -in ./videos/hall_qcif_bw.yuv -out ./videos/q3_n2.yuv -y_res 144 -x_res 176 -frames 10 -o both -q 3 -i 8 -n 2 -r 4 &
 
-# python main.py -in ./videos/hall_qcif_bw.yuv -out ./videos/q3_n3_hall.yuv -y_res 144 -x_res 176 -frames 10 -o both -q 3 -i 8 -n 3 -r 4 &
+python main.py -in ./videos/hall_qcif_bw.yuv -out ./videos/q3_n3.yuv -y_res 144 -x_res 176 -frames 10 -o both -q 3 -i 8 -n 3 -r 4 &
